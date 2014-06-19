7d551abc6c1ae8fc2400b8f1742a009ced26ee3d
zhoujiafei <zhoujiafei@RD-Stephen-Chow.local>
2014-06-19 16:58:25 +0800

    增加字体
    改造成类

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('FilletIconByGD.php');
+
+$icon = new FilletIcon(array(
+	'text' => 'ÖÜ',
+	'radius' => 25,
+	'iconWidth' => 140,
+	'iconHeight' => 140,
+	
+));
+
+$icon->create();
+
+
