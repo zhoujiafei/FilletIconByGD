b63de27268b5d913e5cfa5f04ab3d81130e3a35f
zhoujiafei <564751169@qq.com>
2014-07-11 14:38:08 +0800

    修改
    修改

--- index.php
+++ index.php
@@ -4,17 +4,10 @@ require_once('FilletIconByGD.php');
 $icon = new FilletIcon(array(
 	'text' => '龙',
 	'radius' => 0,
-<<<<<<< HEAD
 	'iconWidth' => 1024,
 	'iconHeight' => 1024,
 	'bgImage' => 'images/1.jpg',
 	'fgImage' => 'images/4.jpg',
-=======
-	'iconWidth' => 128,
-	'iconHeight' => 128,
-	'bgImage' => 'images/5.png',
-	'fgImage' => 'images/model_icon_discover.png',
->>>>>>> FETCH_HEAD
 	'rate' 	=> 0.618,
 	'gradualMode' => 'vertical',
 	'textSize' => 600,
